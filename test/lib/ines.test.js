6fd12e36db083e0084d702e0cb1b0a7a7cb3ddc2
michuNEEO <michael@neeo.com>
2017-05-23 22:58:02 +0200

    update ines reader

--- test/lib/ines.test.js
+++ test/lib/ines.test.js
@@ -6,26 +6,52 @@ import ines from '../../lib/ines';
 test('should read file, read croom.nes', t => {
   return ines.loadRom('./rom/croom.nes')
     .then((result) => {
-      console.log(result);
-      t.is(result.programRomPages, 1);
-      t.is(result.characterRomPages, 1);
+      const romMetaData = result.romMetaData;
+      console.log(romMetaData);
+      t.is(romMetaData.programRomPages, 1);
+      t.is(romMetaData.programRamPages, 1);
+      t.is(romMetaData.characterRomPages, 1);
+      t.is(romMetaData.mapperType, 0);
+      t.is(romMetaData.mirrorType, 1);
+      t.is(romMetaData.batteryBackedPrgRam, false);
+      t.is(romMetaData.trainerIncluded, false);
+      const romData = result.romData;
+      t.is(romData.programRom.length, romMetaData.programRomPages * 16384);
+      t.is(romData.programRam.length, romMetaData.programRamPages * 8192);
+      t.is(romData.characterRom.length, romMetaData.characterRomPages * 8192);
     });
 });
 
 test('should read file, read thwaite.nes', t => {
   return ines.loadRom('./rom/thwaite.nes')
     .then((result) => {
-      console.log(result);
-      t.is(result.programRomPages, 1);
-      t.is(result.characterRomPages, 1);
+      const romMetaData = result.romMetaData;
+      console.log(romMetaData);
+      t.is(romMetaData.programRomPages, 1);
+      t.is(romMetaData.programRamPages, 1);
+      t.is(romMetaData.characterRomPages, 1);
+      t.is(romMetaData.mapperType, 0);
+      t.is(romMetaData.mirrorType, 1);
+      t.is(romMetaData.batteryBackedPrgRam, false);
+      t.is(romMetaData.trainerIncluded, false);
     });
 });
 
 test('should read file, read PwnAdventureZ-csaw-withkeys.nes', t => {
   return ines.loadRom('./rom/PwnAdventureZ-csaw-withkeys.nes')
     .then((result) => {
-      console.log(result);
-      t.is(result.programRomPages, 16);
-      t.is(result.characterRomPages, 0);
+      const romMetaData = result.romMetaData;
+      console.log(romMetaData);
+      t.is(romMetaData.programRomPages, 16);
+      t.is(romMetaData.programRamPages, 1);
+      t.is(romMetaData.characterRomPages, 0);
+      t.is(romMetaData.mapperType, 1);
+      t.is(romMetaData.mirrorType, 0);
+      t.is(romMetaData.batteryBackedPrgRam, true);
+      t.is(romMetaData.trainerIncluded, false);
+      const romData = result.romData;
+      t.is(romData.programRom.length, romMetaData.programRomPages * 16384);
+      t.is(romData.programRam.length, romMetaData.programRamPages * 8192);
+      t.is(romData.characterRom.length, romMetaData.characterRomPages * 8192);
     });
 });
