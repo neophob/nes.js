8f286fb915cf9163b2ab572fac9809e4b4c10f67
Michael Vogt <michael@neeo.com>
2017-05-29 20:24:27 +0200

    fix stack direction

--- test/lib/cpu.test.js
+++ test/lib/cpu.test.js
@@ -12,24 +12,21 @@ test.beforeEach(t => {
 test('should push and pop 8bit data to/from the stack, 0x00', t => {
   t.plan(2);
   t.context.pushStack8(0x00);
-  const result = t.context.popStack8();
-  t.is(result, 0x00);
+  t.is(t.context.popStack8(), 0x00);
   t.is(t.context.registerSP, 0x100);
 });
 
 test('should push and pop 8bit data to/from the stack, 0xfff', t => {
   t.plan(2);
   t.context.pushStack8(0xfff);
-  const result = t.context.popStack8();
-  t.is(result, 0xff);
+  t.is(t.context.popStack8(), 0xff);
   t.is(t.context.registerSP, 0x100);
 });
 
 test('should push and pop 16bit data to/from the stack', t => {
   t.plan(2);
   t.context.pushStack16(0xffaa);
-  const result = t.context.popStack16();
-  t.is(result, 0xffaa);
+  t.is(t.context.popStack16(), 0xffaa);
   t.is(t.context.registerSP, 0x100);
 });
 
@@ -37,7 +34,7 @@ test('should calculate the correct stack pointer address', t => {
   t.context.pushStack16(0xffaa);
   t.context.pushStack8(0);
   t.context.pushStack16(0);
-  t.is(t.context.registerSP, 0x100 + 5);
+  t.is(t.context.registerSP, 0x100 - 5);
 });
 
 test('should transform register P, 0x00', t => {
