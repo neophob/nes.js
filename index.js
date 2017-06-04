fa8457a133a7a32c51ac976ae187935b45e759f9
Michael Vogt <michael@neeo.com>
2017-06-04 14:30:14 +0200

    show memory content in browser

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Nes = require('./lib/nes');
+const client = require('./lib/client');
 
 const romPath = process.argv[2];
 
@@ -9,12 +10,21 @@ if (!romPath) {
   process.exit(1);
 }
 
+
 Nes.loadRom(romPath)
   .then((nes) => {
     nes.start();
+    setInterval(() => {
+      let count = 240;
+      while (count--) {
+        nes.executeCycle();        
+      }
+      client.sendMemData(nes.memory.ram);
+    }, 1000/60);
+
   })
   .catch((error) => {
     console.log('loading rom failed', error);
   });
 
-setTimeout(() => {}, 2000);
+//setTimeout(() => {}, 2000);
